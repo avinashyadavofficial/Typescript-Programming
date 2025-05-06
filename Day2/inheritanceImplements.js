17e75fad1305e4a1580e4ef350eaed06ca14237c
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-06 14:42:43 +0530

    [Avinash Yadav]: Day2: Add inheritance using implements

---
+++ Day2/inheritanceImplements.js
@@ -0,0 +1,12 @@
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var rect = new Rectangle(3, 6);
+console.log(rect.getArea());
