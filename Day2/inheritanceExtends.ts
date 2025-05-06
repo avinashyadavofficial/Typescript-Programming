26c09f031c6f32725bf1d53877b264f38456d8b4
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-06 14:43:02 +0530

    [Avinash Yadav]: Day2: Add inheritance using extends

---
+++ Day2/inheritanceExtends.ts
@@ -0,0 +1,16 @@
+interface Shape{
+    getArea():number;
+}
+class Rectangle implements Shape{
+    public constructor(protected readonly width:number,protected readonly height:number){}
+    public getArea():number{
+        return this.width*this.height;
+    }
+}
+class Square extends Rectangle{
+    public constructor(protected width:number){
+        super(width,width);
+    }
+}
+const sqr=new Square(3);
+console.log(sqr.getArea());//9
