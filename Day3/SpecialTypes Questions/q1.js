4484feb19841d3c13649c0790057e6781d7f4929
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-07 12:51:53 +0530

    [Avinash Yadav]:Day3:Add Declare a variable with any and assign different types to it.

---
+++ Day3/SpecialTypes Questions/q1.js
@@ -0,0 +1,9 @@
+// Declare a variable with any and assign different types to it.
+var x = "avinash";
+var y = 12;
+var z = null;
+var a = true;
+console.log(x); //avinash
+console.log(y); //12
+console.log(z); //null
+console.log(a); //true
---
+++ Day3/SpecialTypes Questions/q1.ts
@@ -0,0 +1,9 @@
+// Declare a variable with any and assign different types to it.
+let x:any="avinash";
+let y:any=12;
+let z:any=null;
+let a:any=true;
+console.log(x);//avinash
+console.log(y);//12
+console.log(z);//null
+console.log(a);//true
