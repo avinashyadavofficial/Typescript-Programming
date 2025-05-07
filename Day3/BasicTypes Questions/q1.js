b6108f119da6a93507a949fd66fec88b158dfc71
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-07 12:48:15 +0530

    [Avinash Yadav]:Day3:Add Declare variables with explicit types: name (string), age (number), isActive (boolean)

---
+++ Day3/BasicTypes Questions/q1.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Declare variables with explicit types: name (string), age (number), isActive (boolean).
+var name = "Avinash";
+var age = 20;
+var isActive = true;
---
+++ Day3/BasicTypes Questions/q1.ts
@@ -0,0 +1,5 @@
+//Declare variables with explicit types: name (string), age (number), isActive (boolean).
+let name:string="Avinash";
+let age:number=20;
+let isActive:boolean=true;
+export{}
