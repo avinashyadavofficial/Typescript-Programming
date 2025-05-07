fb8b4a81632504fdd477b7efa524351a945b8eec
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-07 12:49:33 +0530

    [Avinash Yadav]:Day3:Add Create a function that returns a greeting using a person's name and age.

---
+++ Day3/BasicTypes Questions/q3.js
@@ -0,0 +1,5 @@
+// Create a function that returns a greeting using a person's name and age.
+function greeting(personName, personAge) {
+    return "Good Morning ".concat(personName, " with age ").concat(personAge, ".");
+}
+console.log(greeting("Avi", 21));
---
+++ Day3/BasicTypes Questions/q3.ts
@@ -0,0 +1,5 @@
+// Create a function that returns a greeting using a person's name and age.
+function greeting(personName:string,personAge:number):string{
+    return`Good Morning ${personName} with age ${personAge}.`;
+}
+console.log(greeting("Avi",21));
