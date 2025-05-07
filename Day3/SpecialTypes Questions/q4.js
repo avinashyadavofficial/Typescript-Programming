eb1d3fb817f341fe911fd20a2c9038c971b397db
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-07 12:53:37 +0530

    [Avinash Yadav]:Day3:Add Write a logging function with void return type.

---
+++ Day3/SpecialTypes Questions/q4.js
@@ -0,0 +1,5 @@
+// Write a logging function with void return type.
+function logMessage(message) {
+    console.log(message);
+}
+logMessage("HelloWorld"); //HelloWorld
---
+++ Day3/SpecialTypes Questions/q4.ts
@@ -0,0 +1,7 @@
+// Write a logging function with void return type.
+function logMessage(message: string): void {
+    console.log(message);
+}
+
+logMessage("HelloWorld"); //HelloWorld
+
