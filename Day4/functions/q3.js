1927eeac45868751f2382f72921a5b80dddf425e
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-08 14:22:38 +0530

    [Avinash Yadav]:Day4:Write an arrow function with a return type

---
+++ Day4/functions/q3.js
@@ -0,0 +1,4 @@
+// Write an arrow function with a return type
+var add = function (a, b) { return a + b; };
+//const add=(a:number,b:number):number=>a+b;
+console.log(add(2, 4)); //6
---
+++ Day4/functions/q3.ts
@@ -0,0 +1,4 @@
+// Write an arrow function with a return type
+const add=(a:number,b:number):number=>{return a+b};
+//const add=(a:number,b:number):number=>a+b;
+console.log(add(2,4));//6
