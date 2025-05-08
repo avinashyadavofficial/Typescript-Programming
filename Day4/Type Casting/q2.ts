e2a0807fc9245e5b53829ef14e002e2e77345223
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-08 14:26:58 +0530

    [Avinash Yadav]:Day4:Add a function that takes an unknown type input and safely checks if it's a string before using it

---
+++ Day4/Type Casting/q2.ts
@@ -0,0 +1,11 @@
+// Safely cast an unknown input to a string with a runtime check
+// Write a function that takes an unknown type input and safely checks if it's a string before using it. 
+// If it's not a string, handle the error gracefully.
+function sol(input:unknown):string{
+    if(typeof input!=="string"){
+        throw new Error("Not a string");
+    }
+    return input;
+}
+console.log(sol(122)+3);
+
