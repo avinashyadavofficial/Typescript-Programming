3909460bf4c18ecc2d2d43608c7c5a4b244eff88
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-08 14:29:35 +0530

    [Avinash Yadav]:Day4:Add change color in custom log levels

---
+++ Day4/Logger/colorInLogLevel.ts
@@ -0,0 +1,52 @@
+const winston = require('winston');
+
+// Define custom levels and matching colors
+const logLevels = {
+  fatal: 0,
+  error: 1,
+  warn: 2,
+  info: 3,
+  debug: 4,
+  trace: 5,
+  avinash: 6,
+};
+
+winston.addColors({
+  fatal: 'bgRed black',
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  debug: 'blue',
+  trace: 'cyan',
+  avinash: 'magenta',
+});
+
+const logger=winston.createLogger({
+    levels:logLevels,
+    level:process.env.LOG_LEVEL || 'avinash',//// log everything from 'avinash' and above
+    format:winston.format.cli(),
+    transports:[
+        new winston.transports.Console(),
+    ],
+});
+//Another way-->
+// const logger = winston.createLogger({
+//     levels: logLevels,
+//     level: 'avinash',
+//     format: winston.format.combine(
+//       winston.format.colorize({ all: true }),
+//       winston.format.printf(({ level, message }) => {
+//         return `[${level}]: ${message}`;
+//       })
+//     ),
+//     transports: [new winston.transports.Console()],
+//   });
+
+// Log test messages
+logger.fatal("Fatal message");
+logger.error("Error message");
+logger.warn("Warning message");
+logger.info("Info message");
+logger.debug("Debug message");
+logger.trace("Trace message");
+logger.avinash("Avinash message");
