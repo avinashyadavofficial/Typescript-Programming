79546c326143e14851f4e679451f18a867abfc97
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-08 14:23:30 +0530

    [Avinash Yadav]:Day4:Add Write a function that returns another function with closure over a variable

---
+++ Day4/functions/q5.ts
@@ -0,0 +1,12 @@
+// Write a function that returns another function with closure over a variable
+// A factory function: function makeCounter() { let count = 0; return () => ++count; }
+function makeCounter(){
+    let count=0;
+    return function(){
+        return ++count;
+    }
+}
+const counter=makeCounter();
+console.log(makeCounter);//[Function: makeCounter]
+console.log(counter());//1
+console.log(counter());//2
