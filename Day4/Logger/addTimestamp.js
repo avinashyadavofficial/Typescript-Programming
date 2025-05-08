619e3f6f6a4d4c36809dcaf2eac273364882907f
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-08 14:30:01 +0530

    [Avinash Yadav]:Day4:Add timestamp in winston logger

---
+++ Day4/Logger/addTimestamp.js
@@ -0,0 +1,24 @@
+var winston = require('winston');
+var _a = winston.format, combine = _a.combine, timestamp = _a.timestamp, json = _a.json;
+//create logger
+var logger = winston.createLogger({
+    levels: winston.config.syslog.levels,
+    level: process.env.LOG_LEVEL || 'info',
+    // format:combine(timestamp(), json()),
+    format: combine(timestamp({
+        format: 'YYYY-MM-DD hh:mm:ss.SSS A', // 2022-01-25 03:23:10.350 PM
+    }), json()),
+    transports: [
+        new winston.transports.Console(),
+    ],
+});
+logger.info("Info message");
+logger.warning("Warning message");
+logger.error("Error message");
+// {"level":"info","message":"Info message","timestamp":"2025-05-08T06:23:03.773Z"}
+// {"level":"warning","message":"Warning message","timestamp":"2025-05-08T06:23:03.775Z"}
+// {"level":"error","message":"Error message","timestamp":"2025-05-08T06:23:03.776Z"}
+//so now it would show after customizing timestamp :
+// {"level":"info","message":"Info message","timestamp":"2025-05-08 11:56:16.680 AM"}
+// {"level":"warning","message":"Warning message","timestamp":"2025-05-08 11:56:16.685 AM"}
+// {"level":"error","message":"Error message","timestamp":"2025-05-08 11:56:16.685 AM"}
